47b25b5fcfce7508886cd2adf34a6415622e3521
alexwaitz <alexwaitz@users.noreply.github.com>
2017-11-19 19:58:41 -0800

    code has been modulized
    the code has been modulized and no longer works from the command line

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-var prompt = require("prompt-sync")()
-
-var human_input = prompt("What directory do you want to use? ")
-
 var glob = require("glob-fs")({ gitignore: true })
-
-//reading the directory
-var getDirectories = function (src) {
-  return glob.readdirSync(src + "/**/*.md")
-}
-
-var list_of_locations = getDirectories(human_input)
-
+var fs = require("fs")
 var showdown = require("showdown")
 var converter = new showdown.Converter()
-
-var fs = require("fs")
-
 var path = require("path")
 
-function ensureDirectoryExistence(filePath) {
-  var dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return true;
+var sauceBucket = {
+  parseDirectories: function (directory) {
+    return glob.readdirSync(directory + "/**/*.md")
+  },
+  ensureDirectoryExistence: function (filePath) {
+    var dirname = path.dirname(filePath)
+    if (fs.existsSync(dirname)) {
+      return true
+    }
+    ensureDirectoryExistence(dirname)
+    fs.mkdirSync(dirname)
+  },
+  convertDirectory: function (directory) {
+    var list_of_locations = this.parseDirectories(directory)
+    for (var i=0; i<list_of_locations.length; i++) {
+      var read = fs.readFileSync(list_of_locations[i], "utf8")
+      list_of_locations[i] = list_of_locations[i].replace(directory, "build")
+      list_of_locations[i] = list_of_locations[i].replace(".md", ".html")
+      this.ensureDirectoryExistence(list_of_locations[i])
+      var write = fs.writeFileSync(list_of_locations[i], converter.makeHtml(read))
+    }
   }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
-}
-
-for (var i=0; i<list_of_locations.length; i++) {
-  
-  //reading files
-  var read = fs.readFileSync(list_of_locations[i], "utf8")
-  
-  list_of_locations[i] = list_of_locations[i].replace(human_input, "build")
-  list_of_locations[i] = list_of_locations[i].replace(".md", ".html")
-  
-  //ensuring directory exists
-  ensureDirectoryExistence(list_of_locations[i])
-  
-  //writing files
-  var write = fs.writeFileSync(list_of_locations[i], converter.makeHtml(read))
 }
