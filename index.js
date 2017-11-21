c03ca9ad3a77122905c2d65567c79bd8c91b827d
alexwaitz <alexwaitz@users.noreply.github.com>
2017-11-20 21:34:54 -0800

    Patched up linking & cleaned settings input
    I ensured that the auto directory page would generate proper links. Also, I made the settings passing scheme more versatile.

--- index.js
+++ index.js
@@ -19,20 +19,12 @@ var sauceBucket = {
     this.ensureDirectoryExistence(dirname)
     fs.mkdirSync(dirname)
   },
-  /*buildTreeHTML: function (directory) {
-    var list_of_locations = this.parseDirectories(directory)
-    var tree = {}
+  convertDirectory: function (settings) {
+    var list_of_locations = this.parseDirectories(settings.readDirectory)
+    dom.window.document.getElementById("title").innerHTML = settings.name
     for (var i=0; i<list_of_locations.length; i++) {
       var read = fs.readFileSync(list_of_locations[i], "utf8")
-      list_of_locations[i] = list_of_locations[i].replace(directory, "build")
-    }
-  },*/
-  convertDirectory: function (directory, name) {
-    var list_of_locations = this.parseDirectories(directory)
-    dom.window.document.getElementById("title").innerHTML = name
-    for (var i=0; i<list_of_locations.length; i++) {
-      var read = fs.readFileSync(list_of_locations[i], "utf8")
-      list_of_locations[i] = list_of_locations[i].replace(directory, "build")
+      list_of_locations[i] = list_of_locations[i].replace(settings.readDirectory, settings.writeDirectory)
       list_of_locations[i] = list_of_locations[i].replace(".md", ".html")
       this.ensureDirectoryExistence(list_of_locations[i])
       dom.window.document.getElementById("container").innerHTML = converter.makeHtml(read)
@@ -40,13 +32,13 @@ var sauceBucket = {
     }
     dom.window.document.getElementById("container").innerHTML = "<h1>List Of Pages</h1>"
     for (var i=0; i<list_of_locations.length; i++) {
-      list_of_locations[i] = list_of_locations[i].replace("build", "")
-      dom.window.document.getElementById("container").innerHTML = dom.window.document.getElementById("container").innerHTML + "<p><a href='" + list_of_locations[i] + "'>" + list_of_locations[i].replace(".html", "") + "</a></p>"
+      list_of_locations[i] = list_of_locations[i].replace(settings.writeDirectory, "")
+      dom.window.document.getElementById("container").innerHTML = dom.window.document.getElementById("container").innerHTML + "<p><a href='" + settings.baseURL + list_of_locations[i] + "'>" + list_of_locations[i].replace(".html", "") + "</a></p>"
     }
-    var write = fs.writeFileSync("build/directory.html", dom.serialize())
+    var write = fs.writeFileSync(settings.writeDirectory + "/directory.html", dom.serialize())
   }
 }
 
-sauceBucket.convertDirectory("raw", "Sauce Bucket")
+//sauceBucket.convertDirectory({"readDirectory":"raw", "writeDirectory":"docs", "name":"Sauce Bucket", "baseURL":"https://alexwaitz.github.io/sauce-bucket"})
 
 module.exports = sauceBucket
