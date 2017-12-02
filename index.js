f2819a2fa8d006b02ccef104effcb1fe7f09ffe6
alexwaitz <alexanderjwaitz@gmail.com>
2017-12-01 16:44:09 -0800

    better console logging
    i added better console.log's to make the cli use return more information on progress

--- index.js
+++ index.js
@@ -22,36 +22,8 @@ var sauceBucket = {
     this.ensureDirectoryExistence(filePath)
     fs.mkdirSync(dirname)
   },
-  printThing: function (text) {
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-    var randomValue = getRandomInt(0, 3)
-    if (randomValue === 0) {
-      console.log(chalk.blue(figlet.textSync(text, {
-        font:"Standard",
-        horizontalLayout:"default",
-        verticalLayout:"default"
-      })))
-    }
-    if (randomValue === 1) {
-      console.log(chalk.red(figlet.textSync(text, {
-        font:"Standard",
-        horizontalLayout:"default",
-        verticalLayout:"default"
-      })))
-    }
-    if (randomValue === 2) {
-      console.log(chalk.yellow(figlet.textSync(text, {
-        font:"Standard",
-        horizontalLayout:"default",
-        verticalLayout:"default"
-      })))
-    }
-  },
   convertDirectory: function (settings) {
+    console.log("-initializing conversion process")
     var day = new Date()
     var dom = new JSDOM(fs.readFileSync(__dirname + "/models/model.html", "utf8"))
     dom.window.document.getElementById("navbar").innerHTML = settings.name
@@ -63,6 +35,7 @@ var sauceBucket = {
     dom.window.document.getElementById("title").innerHTML = settings.name
     for (var i=0; i<list_of_locations.length; i++) {
       var read = fs.readFileSync(list_of_locations[i], "utf8")
+      console.log("-converting " + list_of_locations[i])
       list_of_locations[i] = list_of_locations[i].replace(settings.readDirectory, settings.writeDirectory)
       list_of_locations[i] = list_of_locations[i].replace(".md", ".html")
       this.ensureDirectoryExistence(list_of_locations[i])
@@ -70,6 +43,7 @@ var sauceBucket = {
       var write = fs.writeFileSync(list_of_locations[i], dom.serialize())
     }
     dom.window.document.getElementById("container").innerHTML = "<h1>List Of Pages</h1>"
+    console.log("-creating auto directory")
     for (var i=0; i<list_of_locations.length; i++) {
       list_of_locations[i] = list_of_locations[i].replace(settings.writeDirectory, "")
       dom.window.document.getElementById("container").innerHTML = dom.window.document.getElementById("container").innerHTML + "<p><a href='" + settings.baseURL + list_of_locations[i] + "'>" + list_of_locations[i].replace(".html", "") + "</a></p>"
@@ -81,17 +55,23 @@ var sauceBucket = {
     var execLocation = process.cwd()
     var readLocation = execLocation + "/" + userArgs[1] + "/"
     if (userArgs[0] === "-b" || userArgs[0] === "--build") {
+      console.log("-reading configs")
       var settings = JSON.parse(fs.readFileSync(readLocation + "settings.json"))
       settings.readDirectory = userArgs[1]
+      console.log("-convert begin")
       sauceBucket.convertDirectory(settings)
-      this.printThing("Done!")
+      console.log("-done!")
     }
     if (userArgs[0] === "-m" || userArgs[0] === "--make") {
+      console.log("-writing out base files")
       var settings = fs.readFileSync("models/model.json")
       fs.writeFileSync(userArgs[1] + "/settings.json", settings)
       var index = fs.readFileSync("models/model.md")
       fs.writeFileSync(userArgs[1] + "/index.md", index)
-      this.printThing("Done!")
+      console.log("-done!")
+    }
+    if (userArgs[0] === undefined) {
+      console.log("\nusage: sbit [--command] [passed variable]\n\nHere are the commands:\n\n-b or --build converts the passed directory into the html version\n-m or --make writes out a template project in the passed directory\n")
     }
   }
 }
